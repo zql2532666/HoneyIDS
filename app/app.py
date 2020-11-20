2abc4dddccfa71b278fae41731c171fd570edc06
aaron <aaronjiteck@gmail.com>
2020-11-20 17:54:59 +0800

    fix some bugs (format of get data not complete)

--- app/app.py
+++ app/app.py
@@ -53,6 +53,10 @@ def index3():
 def deploy():
     return render_template("deploy.html", title="Honeypot Deployment")
 
+@app.route("/nodes")
+def nodes():
+    return render_template("nodes.html", title="Nodes")
+
 # Uncomment for testing
 # @app.route("/test", methods=['GET', 'POST'])
 # def test():
@@ -94,6 +98,20 @@ def retrieveNode(token):
     return str(json_data)
 
 # Create honeynode
+# Change accordingly with derek's data
+# Current data format to insert
+# {
+#        "honeynode_name" : "test",
+#        "ip_addr" : "192.168.1.2",
+#        "subnet_mask" : "255.255.255.0",
+#        "honeypot_type" : "test",
+#        "nids_type" : "null",
+#        "no_of_attacks" : "2",
+#        "date_deployed" : "2020-01-01 10:10:10",
+#        "heartbeat_status" : "down",
+#        "last_heard" : "idk",
+#        "token" : "2"
+# }
 @app.route("/api/v1/honeynodes/", methods=['POST'])
 def createNode():
 
@@ -111,10 +129,11 @@ def createNode():
     no_of_attacks = request.json['no_of_attacks']
     date_deployed = request.json['date_deployed']
     heartbeat_status = request.json['heartbeat_status']
+    last_heard = request.json['last_heard']
     token = request.json['token']
 
-    sql = f"insert into nodes(honeynode_name, ip_addr, subnet_mask, honeypot_type, nids_type, no_of_attacks, date_deployed, heartbeat_status, token) \
-        values('%s', '%s', '%s', '%s', '%s', %d, '%s', '%s', '%s')" % (honeynode_name, ip_addr, subnet_mask, honeypot_type, nids_type, int(no_of_attacks), date_deployed, heartbeat_status, token)
+    sql = f"insert into nodes(honeynode_name, ip_addr, subnet_mask, honeypot_type, nids_type, no_of_attacks, date_deployed, heartbeat_status, token, last_heard) \
+        values('%s', '%s', '%s', '%s', '%s', %d, '%s', '%s', '%s', '%s')" % (honeynode_name, ip_addr, subnet_mask, honeypot_type, nids_type, int(no_of_attacks), date_deployed, heartbeat_status, token, last_heard)
     
     resultValue = 0
 
@@ -133,26 +152,32 @@ def createNode():
 # Update honeynode
 @app.route("/api/v1/honeynodes/<string:token>", methods=['PUT'])
 def updateNode(token):
-
-    if not request.json or not 'token' in request.json:
+    
+    if not request.json or not token:
         abort(400)
 
     #Mysql connection
     cur = mysql.connection.cursor()
 
-    honeynode_name = request.json['honeynode_name']
-    ip_addr = request.json['ip_addr']
-    subnet_mask = request.json['subnet_mask']
-    honeypot_type = request.json['honeypot_type']
-    nids_type = request.json['nids_type']
-    no_of_attacks = request.json['no_of_attacks']
-    date_deployed = request.json['date_deployed']
-    heartbeat_status = request.json['heartbeat_status']
-    token = request.json['token']
+    honeynode_name = '' if not request.json.__contains__('honeynode_name') else request.json['honeynode_name']
+    ip_addr = '' if not request.json.__contains__('ip_addr') else request.json['ip_addr']
+    subnet_mask = '' if not request.json.__contains__('subnet_mask') else request.json['subnet_mask']
+    honeypot_type = '' if not request.json.__contains__('honeypot_type') else request.json['honeypot_type']
+    nids_type = '' if not request.json.__contains__('nids_type') else request.json['nids_type']
+    no_of_attacks = '' if not request.json.__contains__('no_of_attacks') else int(request.json['no_of_attacks'])
+    date_deployed = '' if not request.json.__contains__('date_deployed') else request.json['date_deployed']
+    heartbeat_status = '' if not request.json.__contains__('heartbeat_status') else request.json['heartbeat_status']
+    last_heard = '' if not request.json.__contains__('last_heard') else request.json['last_heard']
 
-    sql = "update nodes set honeynode_name='%s', ip_addr='%s', subnet_mask='%s', honeypot_type='%s', \
-        nids_type='%s', no_of_attacks=%d, date_deployed='%s', heartbeat_status='%s' where token='%s'" \
-            % (honeynode_name, ip_addr, subnet_mask, honeypot_type, nids_type, int(no_of_attacks), date_deployed, heartbeat_status, token)
+    sql = f"update nodes set honeynode_name=IF('{honeynode_name}' = '', honeynode_name, '{honeynode_name}'), \
+        ip_addr=IF('{ip_addr}' = '', ip_addr, '{ip_addr}'), \
+        subnet_mask=IF('{subnet_mask}' = '', subnet_mask, '{subnet_mask}'), \
+        honeypot_type=IF('{honeypot_type}' = '', honeypot_type, '{honeypot_type}'), \
+        nids_type=IF('{nids_type}' = '', nids_type, '{nids_type}'), \
+        no_of_attacks=IF('{no_of_attacks}' = '', no_of_attacks, '{no_of_attacks}'), \
+        date_deployed=IF('{date_deployed}' = '', date_deployed, '{date_deployed}'), \
+        heartbeat_status=IF('{heartbeat_status}' = '', heartbeat_status, '{heartbeat_status}'), \
+        last_heard=IF('{last_heard}' = '', last_heard, '{last_heard}') where token='{token}'"
     
     resultValue = 0
 
@@ -164,7 +189,7 @@ def updateNode(token):
         print(err)
 
     if resultValue == 0:
-        abort(404)
+        abort(404, "no values inserted")
 
     return jsonify({'success': True}), 200
 
