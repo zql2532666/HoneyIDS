cd5b04c796dadeae139c234c55225ef58e7d29d5
root <zql2532666@gmail.com>
2020-12-20 17:12:28 +0800

    uploaded deployment scripts and honeyagent files

---
+++ app/deployment_scripts/deploy_dionaea.sh
@@ -0,0 +1,184 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [ $# -ne 3 ]
+    then
+        echo "Wrong number of arguments supplied."
+        echo "Usage: $0 <server_ip> <honeynode_token> <honeynode_name>"
+        exit 1
+fi
+
+SERVER_IP=$1
+TOKEN=$2
+HONEYNODE_NAME=$3
+
+INTERFACE=$(basename -a /sys/class/net/e*)
+IP_ADDR=$(ip addr show dev $INTERFACE | grep "inet" | awk 'NR==1{print $2}' | cut -d '/' -f 1)
+SUBNET=$(ifconfig $INTERFACE | grep "Mask:" | awk '{print $4}' | cut -d ':' -f 2)
+DEPLOY_DATE=$(date +"%Y-%m-%d %T")
+
+# stop and disable all services that use package management. 
+function killService() {
+    service=$1
+    sudo systemctl stop $service
+    sudo systemctl kill --kill-who=all $service
+
+    # Wait until the status of the service is either exited or killed.
+    while ! (sudo systemctl status "$service" | grep -q "Main.*code=\(exited\|killed\)")
+    do
+        sleep 10
+    done
+}
+
+function disableTimers() {
+    sudo systemctl disable apt-daily.timer
+    sudo systemctl disable apt-daily-upgrade.timer
+}
+
+function killServices() {
+    killService unattended-upgrades.service
+    killService apt-daily.service
+    killService apt-daily-upgrade.service
+}
+
+function main() {
+    disableTimers
+    killServices
+}
+
+main
+
+
+# Install dependencies
+apt update
+apt --yes install \
+    git \
+    supervisor \
+    build-essential \
+    cmake \
+    check \
+    cython3 \
+    libcurl4-openssl-dev \
+    libemu-dev \
+    libev-dev \
+    libglib2.0-dev \
+    libloudmouth1-dev \
+    libnetfilter-queue-dev \
+    libnl-3-dev \
+    libpcap-dev \
+    libssl-dev \
+    libtool \
+    libudns-dev \
+    python3 \
+    python3-dev \
+    python3-bson \
+    python3-yaml \
+    python3-boto3 \
+    python-pip \
+    python3-pip \
+    curl
+
+pip install configparser
+
+# install honeyagent
+mkdir /opt/honeyagent
+cd /opt/honeyagent
+wget http://$SERVER_IP:5000/api/v1/deployment_script/honeyagent -O honeyagent.py
+wget http://$SERVER_IP:5000/api/v1/deployment_script/honeyagent_conf_file -O honeyagent.conf
+
+# populate the honeyagent config file
+sed -i "s/TOKEN:/TOKEN: $TOKEN/g" honeyagent.conf
+sed -i "s/HONEYNODE_NAME:/HONEYNODE_NAME: $HONEYNODE_NAME/g" honeyagent.conf
+sed -i "s/IP:/IP: $IP/g" honeyagent.conf
+sed -i "s/SUBNET_MASK:/SUBNET_MASK: $SUBNET/g" honeyagent.conf
+sed -i "s/HONEYPOT_TYPE:/HONEYPOT_TYPE: dionaea/g" honeyagent.conf
+sed -i "s/NIDS_TYPE:/NIDS_TYPE: snort/g" honeyagent.conf
+sed -i "s/DEPLOYED_DATE:/DEPLOYED_DATE: $DEPLOY_DATE/g" honeyagent.conf
+sed -i "s/SERVER_IP:/SERVER_IP: $SERVER_IP/g" honeyagent.conf
+    
+cd ~
+git clone https://github.com/zql2532666/dionaea.git
+cd dionaea
+
+# Latest tested version with this install script
+git checkout baf25d6
+
+# api call to join the honeynet
+curl -X POST -H "Content-Type: application/json" -d "{
+	\"honeynode_name\" : \"$HONEYNODE_NAME\",
+	\"ip_addr\" : \"$IP_ADDR\",
+	\"subnet_mask\" : \"$SUBNET\",
+	\"honeypot_type\" : \"dionaea\",
+	\"nids_type\" : \"snort\",
+	\"no_of_attacks\" : \"0\",
+	\"date_deployed\" : \"$DEPLOY_DATE\",
+	\"heartbeat_status\" : \"False\",
+	\"last_heard\" : \"$DEPLOY_DATE\",
+	\"token\" : \"$TOKEN\"
+}" http://$SERVER_IP:5000/api/v1/honeynodes/
+
+
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/opt/dionaea ..
+
+make
+make install
+
+# hpfeeds config
+HPF_HOST=$SERVER_IP  
+HPF_PORT=$(cat /opt/honeyagent/honeyagent.conf | grep "HPFEEDS_PORT" | awk -F: '{print $2}' | xargs)
+HPF_IDENT=$TOKEN
+HPF_SECRET=$TOKEN
+
+cat > /opt/dionaea/etc/dionaea/ihandlers-enabled/hpfeeds.yaml <<EOF
+- name: hpfeeds
+  config:
+    # fqdn/ip and port of the hpfeeds broker
+    server: "$HPF_HOST"
+    port: $HPF_PORT
+    ident: "$HPF_IDENT"
+    secret: "$HPF_SECRET"
+    # dynip_resolve: enable to lookup the sensor ip through a webservice
+    dynip_resolve: "http://canhazip.com/"
+    # Try to reconnect after N seconds if disconnected from hpfeeds broker
+    # reconnect_timeout: 10.0
+EOF
+
+
+# Editing configuration for Dionaea.
+mkdir -p /opt/dionaea/var/log/dionaea/wwwroot /opt/dionaea/var/log/dionaea/binaries /opt/dionaea/var/log/dionaea/log
+chown -R nobody:nogroup /opt/dionaea/var/log/dionaea
+
+mkdir -p /opt/dionaea/var/log/dionaea/bistreams 
+chown nobody:nogroup /opt/dionaea/var/log/dionaea/bistreams
+
+# Config for supervisor.
+cat > /etc/supervisor/conf.d/dionaea.conf <<EOF
+[program:dionaea]
+command=/opt/dionaea/bin/dionaea -c /opt/dionaea/etc/dionaea/dionaea.cfg
+directory=/opt/dionaea/
+stdout_logfile=/opt/dionaea/var/log/dionaea.out
+stderr_logfile=/opt/dionaea/var/log/dionaea.err
+autostart=true
+autorestart=true
+redirect_stderr=true
+stopsignal=QUIT
+EOF
+
+# configure supervisor for honeyagent
+cat > /etc/supervisor/conf.d/honeyagent.conf <<EOF
+[program:honeyagent]
+command=python3 /opt/honeyagent/honeyagent.py
+directory=/opt/honeyagent
+stdout_logfile=/opt/honeyagent/honeyagent.out
+stderr_logfile=/opt/honeyagent/honeyagent.err
+autostart=true
+autorestart=true
+redirect_stderr=true
+stopsignal=QUIT
+EOF
+
+supervisorctl update
---
+++ app/heartbeats_server.py
@@ -0,0 +1,242 @@
+"""
+Author: Thein Than Zaw 
+Date Started: 16/11/2020
+
+The client program, running on any number of computers, periodically sends 
+an UDP packet to the server program that runs on one central computer. 
+
+In the server program, one thread dynamically builds and updates 
+a dictionary that stores the token number of the honeynodes  to the
+timestamp of the last packet received from each and heartbeat_status.
+
+"token" : {
+    heartbeat_status: True,
+    last_heard: time() --> need to format this 
+}
+
+At the same time, the main thread of the server program periodically 
+checks the dictionary, noting whether any of the timestamps is older 
+than a defined timeout. 
+
+The packets are sent from each client per HELL_INTERVAL, while the server 
+checks the dictionary as per DEAD_INTERVAL, and its timeout defaults to the 
+same interval.
+
+These parameters, along with the server IP number and port used, can be 
+adapted to one’s needs. 
+
+HeartBeat server: receives and tracks UDP packets from all clients.
+
+While the heart_beat_log thread logs each UDP packet in a dictionary, the main
+thread periodically scans the dictionary and prints the IP addresses of the
+clients that sent at least one packet during the run, but have
+not sent any packet since a time longer than the definition of the timeout.
+
+Adjust the constant parameters as needed, or call as:
+    PyHBServer.py [timeout [udpport]]
+"""
+
+"""
+
+1)  When the server is started, it will call the http endpoint on the flask server to 
+    retrive the honeynodes details
+        populate_heartbeat_dict()
+       
+2)  The honeynode heartbeat_status is updated every DEAD_INTERVAL
+        update_heartbeat_status()
+
+3)  When a new node is added, the flask API endpoint open a socket connection on the HB_PORT and 
+    send a msg, "POPULATE", this will cause the heartbeat server to run the populate_heartbeat_dict() function
+    again.
+
+populate_heartbeat_dict() populates the heartbeat_dict with all the active nodes
+    1) at the start of the program
+    2) when a new handshake process with the a honeynode is successful (new honeynode added or old honeynode becoming active again)
+        (flask should open a socket connection with a command, and this command should prompt in
+        running of this method)
+
+        Flask: When the endpoint for adding a new node gets run
+            1) checks whether the token exists in the database
+                if exists:
+                    update the heartbeats status 
+                    send msg to heartbeats server to populate the heartbeats dict
+                else:
+                    add new honeynode 
+                    send msg to heartbeats server to populate the heartbeats dict
+
+"""
+
+
+"""
+    You'll often want your threads to be able to use or modify variables common between 
+    threads but to do that you'll have to use something known as a lock.
+    
+     Whenever a function wants to modify a variable, it locks that variable. 
+     
+     When another function wants to use a variable, it must wait until that variable is unlocked.
+"""
+
+# from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM, SOCK_STREAM
+import socket
+from threading import Lock, Thread, Event
+from time import time, ctime, sleep
+import sys
+import json
+import requests
+from configparser import ConfigParser
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+config = ConfigParser()
+config.read(os.path.join(basedir, 'heartbeats_server.conf'))
+
+HBPORT = int(config['HEARTBEATS']['SERVER_HB_PORT']) 
+HELLO_INTERVAL = int(config['HEARTBEATS']['HELLO_INTERVAL'])   
+DEAD_INTERVAL = int(config['HEARTBEATS']['DEAD_INTERVAL'])   
+WEB_SERVER_IP = config['WEB-SERVER']['SERVER_IP'] 
+WEB_SERVER_PORT = config['WEB-SERVER']['PORT']
+
+API_ENDPOINT = f"http://{WEB_SERVER_IP}:{WEB_SERVER_PORT}/api/v1/heartbeats"
+
+class HeartBeatDict:
+    "Manage heartbeat dictionary"
+
+    def __init__(self):
+        self.heartbeat_dict = {}
+        self.heartbeat_dict_lock = Lock()
+
+    # this updates the last_heard_time for each indiv nodes
+    def update_last_heard(self, entry):
+        "Create or update a dictionary entry"
+        print("update_last_heard")
+        self.heartbeat_dict_lock.acquire()
+        for key in self.heartbeat_dict.keys():
+            if entry == key:
+                self.heartbeat_dict[entry]['last_heard'] = time()
+        self.heartbeat_dict_lock.release()
+
+    # this updates the heartbeat_status of all the nodes in the heartbeat_dict after the dead interval
+    def update_heartbeat_status(self, dead_nodes_list):
+        self.heartbeat_dict_lock.acquire()
+        for key in self.heartbeat_dict.keys():
+            # if the node's token can be found in the slient_node_list, the status will be set to False (Dead)
+            if key in dead_nodes_list:
+                self.heartbeat_dict[key]['heartbeat_status'] = False
+            else:
+            # All other nodes, not found in the dead_node_list is set to True (Active)
+                self.heartbeat_dict[key]['heartbeat_status'] = True
+        print(f"\n\nupdate_heartbeat_status\n {self.heartbeat_dict}")
+        heartbeat_json = json.dumps(self.heartbeat_dict)
+        self.heartbeat_dict_lock.release()
+
+        """ perform api call to flask here """
+        try:
+            headers = {'content-type': 'application/json'}
+            requests.post(API_ENDPOINT, data=heartbeat_json, headers=headers)
+        except Exception as err:
+            print(err)
+
+
+    def extract_dead_nodes(self, time_limit):
+        "Returns a list of entries older than time_limit"
+        dead = []
+        when = time() - time_limit
+        self.heartbeat_dict_lock.acquire()
+        for key in self.heartbeat_dict.keys():
+            if self.heartbeat_dict[key]['last_heard'] < when:
+                dead.append(key)
+        self.heartbeat_dict_lock.release()
+        return dead
+    
+    # Need to find a way to call this again when a new node is added
+    def populate_heartbeat_dict(self):
+        self.heartbeat_dict_lock.acquire()
+        print("populating heartbeat dict")
+        """ Perform API Call here """
+        try:
+            r = requests.get(API_ENDPOINT)
+            print(f"populate data successful --> {r.status_code}")
+            if r.json():
+                self.heartbeat_dict = r.json()
+            print(f"populated heartbeat dict {self.heartbeat_dict}")
+        except Exception as err:
+            print(err)
+        self.heartbeat_dict_lock.release()
+
+class HeartBeatReceiver(Thread):
+    "Receive UDP packets, log them in heartbeat dictionary"
+
+    def __init__(self, go_on_event, update_dict_func, port, populate_dict_func):
+        Thread.__init__(self)
+        self.go_on_event = go_on_event
+        self.update_dict_func = update_dict_func
+        self.port = port
+        self.populate_dict_func = populate_dict_func
+        self.receive_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.receive_socket.bind(('', port))
+
+    def __repr__(self):
+        return "Heartbeat Server on port: %d\n" % self.port
+
+    def run(self):
+        while self.go_on_event.isSet():
+            data, addr = self.receive_socket.recvfrom(1024)
+            data = data.decode('utf-8')
+            data = json.loads(data)
+            print (f"Received packet from {addr}") 
+            try:
+                if data['msg'] == "HEARTBEAT":
+                    self.update_dict_func(data['token'])
+                elif data['msg'] == "POPULATE":
+                    # POPULATE Might give us some issues, since it might reset the heartbeat status of 
+                    # the nodes to False
+                    self.populate_dict_func()
+                    
+            except KeyError:
+                print("data received is in wrong format")
+
+def main():
+    "Listen to the heartbeats and detect inactive clients"
+    global HBPORT, DEAD_INTERVAL
+    if len(sys.argv)>1:
+        HBPORT=sys.argv[1]
+    if len(sys.argv)>2:
+        DEAD_INTERVAL=sys.argv[2]
+
+    print (f"HeartBeats server listening on port {HBPORT}") 
+    print ("\n*** Press Ctrl-C to stop ***\n")
+    # Event() --> The internal flag is initially false
+    heat_beat_rec_go_on_event = Event()
+
+    # Set the internal flag to true. All threads waiting for it to become true are awakened. Threads that call wait() once the flag is true will not block at all. 
+    heat_beat_rec_go_on_event.set()
+    heartbeat_dict_object = HeartBeatDict()
+    heartbeat_dict_object.populate_heartbeat_dict()
+    heat_beat_rec_thread = HeartBeatReceiver(heat_beat_rec_go_on_event, heartbeat_dict_object.update_last_heard, HBPORT, heartbeat_dict_object.populate_heartbeat_dict)
+    heat_beat_rec_thread.start()
+    sleep(DEAD_INTERVAL)
+    while 1:
+        try:
+            dead = heartbeat_dict_object.extract_dead_nodes(DEAD_INTERVAL)
+            if dead:
+                print("Dead Nodes")
+                print(f"{dead}")
+                
+            # update the heartbeat_dict here
+            heartbeat_dict_object.update_heartbeat_status(dead)
+            sleep(DEAD_INTERVAL)
+        except KeyboardInterrupt:
+            print ("Exiting.")
+
+            # Reset the internal flag to false. Subsequently, threads calling wait() will block until set() is called to set the internal flag to true again. 
+            heat_beat_rec_go_on_event.clear()
+            heat_beat_rec_thread.join()
+            sys.exit(0)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
