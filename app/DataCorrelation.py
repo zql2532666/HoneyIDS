bd2564dd9f928c69ff2002399d9355dac579bc3b
kygahd <ttz2000@gmail.com>
2021-01-14 21:56:18 +0800

    app.py

--- app/DataCorrelation.py
+++ app/DataCorrelation.py
@@ -5,24 +5,38 @@ from datetime import datetime
 from configparser import ConfigParser
 import os
 
+"""
+dataset = [
+    {
+        date: "",
+        source_ip: "",
+        source_port: "",
+        destination_ip: "",
+        destination_port: "",
+        token: "",
+        honeynode_name: ""
+    }
+]
+"""
 class DataCorrelator():
     def __init__(self,time_window):
         self.time_window = time_window
-
-    def correlate_via_time(self,time_window):
         """
             +   Time Window can be specified by the user
             +   Pull the logs within this time window and process them
         """
-        print("Time")
+    # def correlate_via_time(self,time_window,data):
+
+    #     print("Time")
     
-    def check_one_attacker_attacking_multiple_honeypots():
+    def check_one_attacker_attacking_multiple_honeypots(self):
         """
-            Same src ip address + Different dest ip address
+            +   Same src ip address + Different dest ip address
         """
+
     
     def check_one_attacker_attacking_same_dest_port(self):
         """
-            Same src ip address + same dest port
+            +   Same src ip address + same dest port
         """
         print("DEST PORT")
