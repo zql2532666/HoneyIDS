ae2ddf4cc3c4e00654dd881d487a8e964b6ba53f
Afrin <afrin@gmail.com>
2022-05-26 18:11:03 +0800

    adding node listing page changes

--- venv3/Scripts/activate.bat
+++ venv3/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\user\Documents\projects\upwork\threatspan\threatspan-deception\venv3
+set VIRTUAL_ENV=C:\Users\syed_\Desktop\TS1\threatspan-deception\venv3
 
 if not defined PROMPT set PROMPT=$P$G
 
